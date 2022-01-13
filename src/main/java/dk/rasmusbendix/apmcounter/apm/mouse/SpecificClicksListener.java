f383176f9865b4a9c9b113be55c5c42eaef16dff
rasm945i <rasmusb8b@gmail.com>
2022-01-13 11:04:11 +0100

    Updated to fit into Moshi so we can JSON-ify our EventWrapper and easily save and load created groups. Yay!

--- src/main/java/dk/rasmusbendix/apmcounter/apm/mouse/SpecificClicksListener.java
+++ src/main/java/dk/rasmusbendix/apmcounter/apm/mouse/SpecificClicksListener.java
@@ -4,14 +4,14 @@ import com.github.kwhat.jnativehook.mouse.NativeMouseEvent;
 import com.github.kwhat.jnativehook.mouse.NativeMouseInputListener;
 import dk.rasmusbendix.apmcounter.apm.ActionEvent;
 
-import java.util.ArrayList;
+import java.util.Set;
 
 public class SpecificClicksListener implements NativeMouseInputListener {
 
-    private ActionEvent event;
-    private ArrayList<Integer> buttons;
+    private final ActionEvent event;
+    private final Set<Integer> buttons;
 
-    public SpecificClicksListener(ActionEvent event, ArrayList<Integer> buttons) {
+    public SpecificClicksListener(ActionEvent event, Set<Integer> buttons) {
         this.buttons = buttons;
         this.event = event;
     }
